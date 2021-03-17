68c19dd7f829ea8b79fa208e6ea65b84f3133605
FaysalShuvo <shuvonma57@gmail.com>
2021-03-17 10:56:10 +0600

    43-3 finished succesfully

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Cart = (props) => {
   const cart = props.cart;
-  //   const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
+
   let totalPrice = 0;
   for (let i = 0; i < cart.length; i++) {
     const prd = cart[i];
-    totalPrice = totalPrice + prd.price;
+    totalPrice = totalPrice + prd.price * prd.quantity;
   }
 
   let shipping = 0;
@@ -18,13 +18,13 @@ const Cart = (props) => {
     shipping = 12.99;
   }
 
-  const tax = (totalPrice / 10);
-    const grandTotal = (totalPrice + shipping + Number(tax)).toFixed(2);
-    
-    const formatNumber = num => {
-        const precision = num.toFixed(2);
-        return Number(precision)
-    }
+  const tax = totalPrice / 10;
+  const grandTotal = (totalPrice + shipping + Number(tax)).toFixed(2);
+
+  const formatNumber = (num) => {
+    const precision = num.toFixed(2);
+    return Number(precision);
+  };
   return (
     <div>
       <h4>Order Summery</h4>
@@ -37,6 +37,8 @@ const Cart = (props) => {
         <small>VAT: {formatNumber(tax)}</small>
       </p>
       <p>Total Price: {grandTotal}</p>
+      <br />
+      {props.children}
     </div>
   );
 };
